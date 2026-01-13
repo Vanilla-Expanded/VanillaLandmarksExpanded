87b36fe5d011f45710183c63cfac13404a1ab844
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-01-13 12:10:43 +0100

    v0.1

---
+++ 1.6/Source/VanillaExplorationExpanded/TileMutatorWorkers/Flora/TileMutatorWorker_PlentifulGrass.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+namespace VanillaExplorationExpanded
+{
+    public class TileMutatorWorker_PlentifulGrass : TileMutatorWorker
+    {
+
+        public TileMutatorWorker_PlentifulGrass(TileMutatorDef def)
+            : base(def)
+        {
+        }
+
+
+        public override float PlantCommonalityFactorFor(ThingDef plant, PlanetTile tile)
+        {
+            return (plant.defName.Contains("Grass")) ? 10f : 1f;
+        }
+
+
+    }
+}
---
+++ 1.6/Source/VanillaExplorationExpanded/TileMutatorWorkers/Mountains/TileMutatorWorker_IceSpires.cs
@@ -0,0 +1,53 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.Noise;
+namespace VanillaExplorationExpanded
+{
+    public class TileMutatorWorker_IceSpires : TileMutatorWorker
+    {
+        private ModuleBase blobs;
+        private const float blobFreq = 0.15f;
+        private const float blobThreshold = 0.2f;
+        private const float warpFreq = 0.02f;
+        private const float warpStrength = 18f;
+
+        public TileMutatorWorker_IceSpires(TileMutatorDef def)
+            : base(def)
+        {
+        }
+
+        public override void Init(Map map)
+        {
+            base.Init(map);
+
+            blobs = new Perlin(blobFreq, 2.0, 0.5, 4, Rand.Int, QualityMode.Medium
+            );
+
+            blobs = MapNoiseUtility.AddDisplacementNoise(blobs, warpFreq, warpStrength
+            );
+
+            blobs = new Translate(-map.Size.x / 2f, 0.0, -map.Size.z / 2f, blobs
+            );
+
+            NoiseDebugUI.StoreNoiseRender(blobs, "icespires");
+        }
+
+        public override void GeneratePostElevationFertility(Map map)
+        {
+           
+
+            foreach (IntVec3 c in map.AllCells)
+            {
+                float v = blobs.GetValue(c);
+
+                if (v > blobThreshold)
+                {
+                    GenSpawn.Spawn(ThingDefOf.SolidIce, c, map);
+                }
+            }
+        }
+
+
+    }
+}
