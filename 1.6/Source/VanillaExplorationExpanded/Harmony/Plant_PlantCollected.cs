5eaec783a8f42b210785f5e692234540210e0562
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-07-01 08:49:05 +0200

    Reapply "Merge branch 'main' of https://github.com/Vanilla-Expanded/VanillaLandmarksExpanded"
    This reverts commit 9e82b361ae07c0c19438c66af0402cb7eb856407.

--- 1.6/Source/VanillaExplorationExpanded/Harmony/Plant_PlantCollected.cs
+++ 1.6/Source/VanillaExplorationExpanded/Harmony/Plant_PlantCollected.cs
@@ -2,10 +2,8 @@
 using RimWorld;
 using System.Reflection;
 using Verse;
-
 using System;
 using UnityEngine;
-
 
 namespace VanillaExplorationExpanded
 {
@@ -21,36 +19,31 @@ namespace VanillaExplorationExpanded
             {
                 if (mutator.Worker != null && mutator.Worker is TileMutatorWorker_PoisonousFlora)
                 {
-                    float sev = 0.05f;
-                    sev *= Mathf.Max(1f - by.GetStatValue(StatDefOf.ToxicResistance), 0f);
-                    if (ModsConfig.BiotechActive)
+                    if (!__instance.IsCrop)
                     {
-                        sev *= Mathf.Max(1f - by.GetStatValue(StatDefOf.ToxicEnvironmentResistance), 0f);
-                    }
-                   
-                    if (sev != 0f)
-                    {
-                        Effecter damageEffecter = InternalDefOf.Impact_Toxic.Spawn();
-                        damageEffecter.Trigger(by, by);
-                        damageEffecter.Cleanup();
-                        HealthUtility.AdjustSeverity(by, HediffDefOf.ToxicBuildup, sev);
-                       
-                    }
+                        float sev = 0.05f;
+                        sev *= Mathf.Max(1f - by.GetStatValue(StatDefOf.ToxicResistance), 0f);
+                        if (ModsConfig.BiotechActive)
+                        {
+                            sev *= Mathf.Max(1f - by.GetStatValue(StatDefOf.ToxicEnvironmentResistance), 0f);
+                        }
+
+                        if (sev != 0f)
+                        {
+                            Effecter damageEffecter = InternalDefOf.Impact_Toxic.Spawn();
+                            damageEffecter.Trigger(by, by);
+                            damageEffecter.Cleanup();
+                            HealthUtility.AdjustSeverity(by, HediffDefOf.ToxicBuildup, sev);
+
+                        }
+                    }              
 
                 }
             }
 
-                   
-
-
-     
-
-
         }
 
-
     }
-
 
 }
 
