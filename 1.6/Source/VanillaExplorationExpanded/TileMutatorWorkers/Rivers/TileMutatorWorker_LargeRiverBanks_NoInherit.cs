e64fe5c5c6df9059b45c4e978e1cecc39d793b57
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-07-01 10:14:07 +0200

    v1.07

---
+++ 1.6/Source/VanillaExplorationExpanded/TileMutatorWorkers/Rivers/TileMutatorWorker_LargeRiverBanks_NoInherit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+using Verse.Noise;
+namespace VanillaExplorationExpanded
+{
+
+    public class TileMutatorWorker_LargeRiverBanks_NoInherit : TileMutatorWorker
+    {
+     
+
+        public TileMutatorWorker_LargeRiverBanks_NoInherit(TileMutatorDef def)
+            : base(def)
+        {
+        }
+
+        public override bool IsValidTile(PlanetTile tile, PlanetLayer layer)
+        {
+            Tile tileInfo = Find.WorldGrid[tile];
+            SurfaceTile surface = tileInfo as SurfaceTile;
+            if (surface != null)
+            {
+                return surface?.Rivers?.Count > 0;
+            }
+            else return false;
+
+        }
+
+        public override string GetLabel(PlanetTile tile)
+        {
+            return def.label;
+        }
+
+      
+
+    }
+}
